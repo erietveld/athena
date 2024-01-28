f763fb53e062f7859dbce88d53804e12f8c6cb31
Eric Rietveld <erietveld@gmail.com>
2024-01-28 21:56:35 +0100

    Works partially

--- script.js
+++ script.js
@@ -1,15 +1,15 @@
 document.addEventListener('DOMContentLoaded', function () {
     const wordElement = document.querySelector('.word');
-    const options = document.querySelector('.options');
     const checkBtn = document.getElementById('checkBtn');
-    const checkBtn2 = document.getElementById('checkBtn2');
+    const firstGroup = document.getElementById('firstGroup');
+    const secondGroup = document.getElementById('extraOptions');
     const optionGroups = document.querySelectorAll('.option-group');
-
-    const questions = [
-        { word: 'woord1', answer: ['nom ev m','nom mv f'] },
-        { word: 'woord2', answer: ['gen mv f']},
-        // Add more questions if needed
-    ];
+    /*
+        const questions = [
+            { word: 'woord1', answer: ['nom ev m','nom mv f'] },
+            { word: 'woord2', answer: ['gen mv f']},
+            // Add more questions if needed
+        ];*/
 
     let selectedAnswers = [];
 
@@ -19,33 +19,37 @@ document.addEventListener('DOMContentLoaded', function () {
     // Selecteer het extra optieblok
     const extraOptions = document.getElementById('extraOptions');
     var tweedeOptie = false;
-
+ 
     // Voeg een event listener toe om te luisteren naar klikgebeurtenissen op de extra optie-knop
-    extraOptionBtn.addEventListener('click', function() {
-        // Als het extra optieblok bestaat
-        
+    extraOptionBtn.addEventListener('click', function () {
 
         if (extraOptions && !tweedeOptie) {
-            // Toon het extra optieblok
-            extraOptions.style.display = 'flex';
-            tweedeOptie = true;
-            extraOptionBtn.innerText = "toch maar 1 optie";
+            setSecondBlock();
         }
-        else{
-            extraOptions.style.display = 'none';
-            tweedeOptie = false;
-            extraOptionBtn.innerText = "nog een optie";
-
-            const extraOptionButtons = extraOptions.querySelectorAll('.option');
-            extraOptionButtons.forEach(button => {
-                button.classList.remove('selected');
-            });
-            selectedAnswers.splice(3);
-            console.log("selected answers" + selectedAnswers);
+        else {
+            resetSecondBlock();
         }
         updateCheckButtonVisibility();
     });
 
+    function setSecondBlock() {
+        // Toon het extra optieblok
+        extraOptions.style.display = 'flex';
+        tweedeOptie = true;
+        extraOptionBtn.innerText = "Toch maar 1 optie";
+    }
+
+    function resetSecondBlock() {
+        extraOptions.style.display = 'none';
+        tweedeOptie = false;
+        extraOptionBtn.innerText = "Nog een optie";
+
+        const extraOptionButtons = extraOptions.querySelectorAll('.option');
+        extraOptionButtons.forEach(button => {
+            button.classList.remove('selected');
+        });
+        selectedAnswers.splice(3);
+    }
 
     function showQuestion() {
         const randomIndex = Math.floor(Math.random() * questions.length);
@@ -53,92 +57,84 @@ document.addEventListener('DOMContentLoaded', function () {
         wordElement.textContent = question.word;
         selectedAnswers = [];
         resetButtons(); // Reset button classes
-        checkBtn.style.display = 'none'; // Hide the "Check" button
+        checkBtn.disabled = true;
+        // checkBtn.style.display = 'none'; // Hide the "Check" button
+    }
+    function verifyTwoAnswers(correctAnswer){        
+        const firstAnswer = selectedAnswers.slice(0, 3).join(" ");
+        const secondAnswer = selectedAnswers.slice(3).join(" ");
+        if ((correctAnswer[1] == firstAnswer) || (correctAnswer[0] == secondAnswer)){
+            verifyOneOptionGroup(firstGroup, correctAnswer[1].split(" "));
+            verifyOneOptionGroup(secondGroup, correctAnswer[0].split(" "));
+        }
+        else {
+            verifyOneOptionGroup(firstGroup, correctAnswer[0].split(" "));
+            verifyOneOptionGroup(secondGroup, correctAnswer[1].split(" "));
+        }
+        setSecondBlock();
     }
 
     function checkAnswer() {
-        const currentWord = wordElement.textContent;
-        const correctAnswer= questions.find(question => question.word === currentWord)?.answer;
-        console.log('Correct Answers:', correctAnswer);
+        const currentWord = wordElement.textContent;  
+        const correctAnswer = questions.find(question => question.word === currentWord)?.answers;
+        console.log('Correct Answer: ', correctAnswer);
+        const correctFirstAnswer = correctAnswer[0].split(" ");
+      //  console.log('Correct AnswerA: ', correctFirstAnswer);
         const selectedAnswersString = selectedAnswers.join(' ');
-        console.log('Selected Answers:', selectedAnswersString);
-    }
-
-    function checkAnswers(){
-        const currentWord = wordElement.textContent;
-        const correctAnswerGroups = questions.find(question => question.word === currentWord)?.answer;
-        console.log('Correct Answers:', correctAnswerGroups);
-        const selectedAnswersString = selectedAnswers.join(' ');
-        console.log('Selected Answers:', selectedAnswersString);
-        const isCorrect = correctAnswers.some(answer => answer === selectedAnswersString);
-
-        
-        if (correctAnswerGroups) {
-            const correctAnswerIndices = correctAnswerGroups.map(answerGroup => {
-                const correctAnswers = answerGroup.split(' ');
-                return correctAnswers.map(answer => selectedAnswers.indexOf(answer));
-            });
-            
-            const selectedAnswerIndices = selectedAnswers.map(answer => answer.split(' ').map(part => {
-                const option = optionGroups.find(group => group.querySelector(`[data-type="${part}"]`));
-                return Array.from(option.querySelectorAll('.option')).indexOf(option.querySelector(`[data-type="${part}"]`));
-            }));
-            
-            let isCorrect = correctAnswerIndices.some(correctIndices =>
-                selectedAnswerIndices.every(selectedIndices =>
-                    correctIndices.every((correctIndex, index) => selectedIndices[index] === correctIndex)
-                )
-            );
-            
-            optionGroups.forEach(group => {
-                const selectedButton = group.querySelector('.selected');
-                const selectedButtonType = selectedButton.dataset.type;
-                const correctButtonType = correctAnswerGroups[0].split(' ')[0];
-                if (selectedButtonType === correctButtonType && isCorrect) {
-                    selectedButton.classList.add('correct');
-                } else if (selectedButton) {
-                    selectedButton.classList.add('incorrect');
-                }
-            });
-        } else {
-            console.log('Question not found for the current word:', currentWord);
+        console.log('Selected Answer: ', selectedAnswersString);
+        if (correctAnswer.length == 2) {
+            verifyTwoAnswers(correctAnswer);
+            return;
         }
-        
-        resetButtonsAfterTimeout(); // Reset button classes after 2 seconds
+
+        verifyOneOptionGroup(firstGroup, correctFirstAnswer);
+
+        if ((selectedAnswers.length > 3) && (correctAnswer.length == 1)){
+            secondGroup.querySelectorAll('.option-group').forEach((optionGroup, index)  => {
+                // Loop over alle opties binnen de huidige optiegroep
+                optionGroup.querySelectorAll('.option').forEach(option => {
+                    option.classList.add('incorrect');
+                    fullAnswerCorrect = false; 
+                });
+            });
+        }
+
+       // resetButtonsAfterTimeout(); // Reset button classes after 2 seconds
     }
 
-    
+    function verifyOneOptionGroup(optionGroup, answerGroep){
+        console.log("answerGroep" + answerGroep);
+        console.log("optionGroup" + optionGroup);
+        var fullAnswerCorrect = true;
+        // Loop over elke optiegroep binnen de eerste groep
+        optionGroup.querySelectorAll('.option-group').forEach((optionGroup, index)  => {
+            // Loop over alle opties binnen de huidige optiegroep
+            const correctSingleAnswer = answerGroep[index];
+            var hasMadeSelection = (optionGroup.querySelectorAll('.option.selected').length > 0);
+            optionGroup.querySelectorAll('.option').forEach(option => {
+                const dataType = option.getAttribute('data-type');
+                const isSelected = option.classList.contains('selected');
+                const isCorrect = (dataType == correctSingleAnswer);
 
-    function checkAnswersOld() {
-        const currentWord = wordElement.textContent;
-        const correctAnswers = questions.find(question => question.word === currentWord)?.answers;
-        console.log('Correct Answers:', correctAnswers);
-        console.log('Selected Answers:', selectedAnswers);
-        
-        if (correctAnswers) {
-            const correctIndices = correctAnswers.map(answer => selectedAnswers.indexOf(answer));
-            console.log('correctIndices', correctIndices);
-            optionGroups.forEach((group, index) => {
-                const selectedButton = group.querySelector('.selected');
-                const correctButton = group.querySelectorAll('.option')[correctIndices[index]];
-                
-                if (selectedButton && correctButton && selectedButton === correctButton) {
-                    selectedButton.classList.add('correct');
-                } else if (selectedButton) {
-                    selectedButton.classList.add('incorrect');
+                // Voeg klassen toe op basis van de resultaten
+                if (isSelected && isCorrect) {
+                    option.classList.add('correct');
+                } else if (isSelected && !isCorrect) {
+                    option.classList.add('incorrect');
+                    fullAnswerCorrect = false; 
                 }
-                
-                if (correctButton) {
-                    correctButton.classList.add('correct');
+                else if (!isSelected && isCorrect) {
+                    fullAnswerCorrect = false; 
+                    option.classList.add('correct');
+                }
+                else if (!hasMadeSelection && !isCorrect){
+                    option.classList.add('incorrect');
                 }
             });
-        } else {
-            console.log('Question not found for the current word:', currentWord);
-        }
-        
-        resetButtonsAfterTimeout(); // Reset button classes after 2 seconds
+        });
+
+
     }
-    
 
     function resetButtons() {
         optionGroups.forEach(group => {
@@ -147,6 +143,7 @@ document.addEventListener('DOMContentLoaded', function () {
                 button.classList.remove('selected', 'correct', 'incorrect');
             });
         });
+        resetSecondBlock();
     }
 
     function resetButtonsAfterTimeout() {
@@ -177,11 +174,11 @@ document.addEventListener('DOMContentLoaded', function () {
     }
     function updateCheckButtonVisibility() {
         // Check if all three answers are selected
-       // checkBtn.style.display = 'none';
+        // checkBtn.style.display = 'none';
+      //  console.log("Updating checkbutotn visiblity");
         checkBtn.disabled = true;
-        if ((selectedAnswers.length === 3 && !tweedeOptie) || 
-           (selectedAnswers.length === 6 && tweedeOptie))
-        {
+        if ((selectedAnswers.length === 3 && !tweedeOptie) ||
+            (selectedAnswers.length === 6 && tweedeOptie)) {
             checkBtn.disabled = false;
             checkBtn.style.display = 'flex'; // Display the "Check" button
         }
